42399413cc1d71d2951c39568efbfbccaccf15f1
Oliver Bandel <oliver@first.in-berlin.de>
2013-01-19 02:32:39 +0100

    startup: base-doc stored as BASEDOC-variable.

--- main.ml
+++ main.ml
@@ -557,7 +557,7 @@ let _  =
                               (* we evaluate the parse-tree, and start with a first, implicit get *)
                               (* with the url we got from the command line                        *)
                               (* ---------------------------------------------------------------- *)
-                              evaluate_command_list (Get_url(url, "-") :: parserdef.commands)
+                              evaluate_command_list (Get_url(url, "-") :: Store("BASEDOC") :: parserdef.commands)
 
 
                             with (* handle exceptions from the parse-tree-evaluation *)
