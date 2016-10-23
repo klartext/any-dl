4ef74a05119b77c960a37b016b582ce0b5f07866
Oliver Bandel <oliver@first.in-berlin.de>
2016-10-23 15:22:11 +0200

    Changed interface because of OCaml 4.03.

--- tools.mli
+++ tools.mli
@@ -35,6 +35,8 @@ module Array2 :
     external set : 'a array -> int -> 'a -> unit = "%array_safe_set"
     external make : int -> 'a -> 'a array = "caml_make_vect"
     external create : int -> 'a -> 'a array = "caml_make_vect"
+    external create_float : int -> float array = "caml_make_float_vect"
+    val make_float : int -> float array
     val init : int -> (int -> 'a) -> 'a array
     val make_matrix : int -> int -> 'a -> 'a array array
     val create_matrix : int -> int -> 'a -> 'a array array
@@ -47,12 +49,16 @@ module Array2 :
     val to_list : 'a array -> 'a list
     val of_list : 'a list -> 'a array
     val iter : ('a -> unit) -> 'a array -> unit
-    val map : ('a -> 'b) -> 'a array -> 'b array
     val iteri : (int -> 'a -> unit) -> 'a array -> unit
+    val map : ('a -> 'b) -> 'a array -> 'b array
     val mapi : (int -> 'a -> 'b) -> 'a array -> 'b array
     val fold_left : ('a -> 'b -> 'a) -> 'a -> 'b array -> 'a
     val fold_right : ('b -> 'a -> 'a) -> 'b array -> 'a -> 'a
-    external make_float : int -> float array = "caml_make_float_vect"
+    val iter2 : ('a -> 'b -> unit) -> 'a array -> 'b array -> unit
+    val map2 : ('a -> 'b -> 'c) -> 'a array -> 'b array -> 'c array
+    val for_all : ('a -> bool) -> 'a array -> bool
+    val mem : 'a -> 'a array -> bool
+    val memq : 'a -> 'a array -> bool
     val sort : ('a -> 'a -> int) -> 'a array -> unit
     val stable_sort : ('a -> 'a -> int) -> 'a array -> unit
     val fast_sort : ('a -> 'a -> int) -> 'a array -> unit
